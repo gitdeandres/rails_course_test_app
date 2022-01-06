8ab233360b80f46c8c3efa76647e1e11e122f0f1
gitdeandres <andresravelo.9@gmail.com>
2022-01-06 16:38:38 -0300

    set validation of articles creation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+    validates :title, presence: true, lenght: {minimun: 6, maximun: 100}
+    validates :description, presence: true, lenght: {minimun: 10, maximun: 300}
 end
